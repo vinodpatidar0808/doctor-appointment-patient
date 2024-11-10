df336042e18198aaf91d7b9b04c70adcd567c373
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-11-10 13:02:08 +0530

    change path from sidebar navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
     <div className="w-[20%] h-full border-r-2 border-charcoalGray fixed top-0 left-0">
       <div className="py-5  flex items-center justify-center">LOGO</div>
       <div className="flex flex-col  gap-1 py-3 text-sm">
-        <NavLink to="/" end className={({ isActive }) =>
+        <NavLink to="/dashboard" end className={({ isActive }) =>
           `flex gap-2 items-center px-8 py-2 ${isActive ? "bg-softGray" : ""}`
         }> <p className="w-3 h-3  border-2 border-charcoalGray"></p> Dashboard</NavLink>
       </div>
